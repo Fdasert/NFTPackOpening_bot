2862338ea284ca422aef3f23f460207d5be3e7b4
Fdasert <fdasert434@gmail.com>
2025-07-01 15:43:46 +0300

    22

--- back/bot/bot.py
+++ back/bot/bot.py
@@ -2,7 +2,7 @@ from telegram import InlineKeyboardMarkup, InlineKeyboardButton, Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = '7942953226:AAHri1jAKi_orylmcE3dGAJ3WwV3C_8NXt4'
-WEB_APP_URL = "https://fdasert.github.io/NFTPackOpening_bot/"
+WEB_APP_URL = "https://nft-pack-opening-bot.vercel.app"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = InlineKeyboardMarkup([
@@ -15,7 +15,7 @@ app.add_handler(CommandHandler("start", start))
 app.run_polling()
 
 if __name__ == '__main__':
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     print("Бот запущен...")
     app.run_polling()
